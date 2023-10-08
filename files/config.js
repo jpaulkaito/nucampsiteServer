fc4e958374988ec8caebdf74d5b81dd3e8fb0908
JimPaul <jpaulkaito@gmail.com>
2023-10-07 18:35:19 -0700

    Passport Facebook

--- files/config.js
+++ files/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '262007779605158',
+        clientSecret: 'dcdff859ac63940bce56b1954c5091c2'
+    }
 }

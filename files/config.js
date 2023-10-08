fad8cee692528fd34c474dd3fed386ad190a059e
jpaulkaito <133739630+jpaulkaito@users.noreply.github.com>
2023-10-07 19:16:51 -0700

    Update config.js

--- files/config.js
+++ files/config.js
@@ -1,8 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '262007779605158',
-        clientSecret: 'dcdff859ac63940bce56b1954c5091c2'
-    }
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
 }
